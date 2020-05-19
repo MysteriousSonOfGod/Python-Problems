e189fa8a2e9d4d6f97420850eabcd04b2abc1ba4
jatta-njie21 <laboyjatta@gmail.com>
2020-05-19 17:36:07 +0000

    First commit for question 3

--- number3.py
+++ number3.py
@@ -1,1 +1,15 @@
+num1 = float(input("Enter a number: "))
+num2 = float(input("Enter a number: "))
+num3 = float(input("Enter a number: "))
 
+if num1 > num2 and num1 > num2:
+    print("The largest number is",num1)
+
+elif num2 > num1 and num2 > num3:
+    print("The largest number is",num2)
+
+elif num3 > num1 and num3 > num2:
+    print("The largest number is",num3)
+
+else:
+    print("Invalid input  ")
