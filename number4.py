cffcb7d1ba96559e9d99b5542e6e9f819c61c8ea
jatta-njie21 <laboyjatta@gmail.com>
2020-05-19 17:49:27 +0000

    Initial commit question4

--- number4.py
+++ number4.py
@@ -1,1 +1,17 @@
+from_unit  = input("Convert from? ")
+to_unit    = input("Convert to? ")
+value_unit      = float(input("Value? "))
 
+def gal_to_ml(gal):
+    result = value_unit * 3785.41
+    print(result,'ml')
+
+
+if from_unit == "gal" and to_unit == "ml":
+    gal_to_ml(from_unit)
+
+elif from_unit == "gal" and to_unit == "km":
+    print("Incompatible conversion")
+
+else:
+    print("Invalid unit entered!!")
