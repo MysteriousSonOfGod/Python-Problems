548c7a68c1b9a4d1ee2f2c5fe7c291f4924f38c7
jatta-njie21 <laboyjatta@gmail.com>
2020-05-24 18:57:16 +0000

    question9

---
+++ w3school/question8.py
@@ -0,0 +1,1 @@
+# 8. Write a Python program to check a list is empty or not
---
+++ w3school/quetion9.py
@@ -0,0 +1,8 @@
+# 9. Write a Python program to clone or copy a list
+
+list1 = ['lamin',2,123,'egg','goat',3442,]
+
+list2 = list1.copy()
+
+print("List1 =",list1)
+print("List2 =",list1)
