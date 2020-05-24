c71444440be26834d6d958e67e8c51b9cbfb9502
jatta-njie21 <laboyjatta@gmail.com>
2020-05-24 19:28:56 +0000

    question8

---
+++ w3school/question10.py
@@ -0,0 +1,3 @@
+# 10. Write a Python program to find the list of words 
+# that are longer than n from a given list of words.
+
--- w3school/question8.py
+++ w3school/question8.py
@@ -1,1 +1,9 @@
 # 8. Write a Python program to check a list is empty or not
+
+#list1 = []
+list2 = [2,3,9,18,11,1]
+
+if len(list2) > 0:
+    print("List is not empty")
+else:
+    print("List is empty ")
