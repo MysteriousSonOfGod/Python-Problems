fb1b4778d962f7240daaa28a0b2c3f70499f43be
jatta-njie21 <laboyjatta@gmail.com>
2020-05-25 01:21:27 +0000

    question15

---
+++ w3school/question15.py
@@ -0,0 +1,11 @@
+# 15. Write a Python program to shuffle and print a specified list.
+
+list1 = ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+
+
+#list3 = list2.reverse()
+
+print(list1)
+print(sorted(list1))
+list1.reverse()
+print(list1)
