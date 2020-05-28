447d53e1552908db5f2ee07ba94a78027150f0b3
jatta-njie21 <laboyjatta@gmail.com>
2020-05-28 14:11:57 +0000

    question16

--- w3school/question11.py
+++ w3school/question11.py
@@ -1,23 +1,15 @@
 # 11. Write a Python function that takes two lists 
 # and returns True if they have at least one common member.
 
+list1 = [1,2,3,4,0,2]
+list2 = [8,9,0,5,3,10]
 
+list3 = set(list1)
+list4 = set(list2)
 
-def compare_lists():
-    list1 = [1,2,3,4,0,2]
-    list2 = [8,9,0,5,3,10]
-
-    for items in list1:
-        for items2 in list2:
-            if items in list2:
-                return True
-            else:
-                return False
-
-    for list2 in list1:
-        print("nakala")
-
-
-
-result = compare_lists()
-print(result)
+for x in list3:
+    for y in list4:
+        if x in list4:
+            print(True)
+        else:
+            print(False)
